75a9d64bb82d4ed6c6a8ea40149fceef8aea178b
Pryshliak <DPryshliak@luxoft.com>
2021-12-07 16:50:10 +0200

    Some changes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ import carsRouter from './api/cars.js'
 
 const app = express()
 const PORT = process.env.PORT || 3000
-const DB_URL = 'mongodb+srv://dima:dima@cluster0.8qv9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const dev_db_url = 'mongodb+srv://dima:dima@cluster0.8qv9h.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const mongoDB = process.env.MONGODB_URI || dev_db_url
 
 app.use(express.json())
 app.use('/api', carsRouter)
@@ -12,7 +13,7 @@ app.use('/api', carsRouter)
 async function startApp() {
     try {
         app.listen(PORT, async () => {
-            await mongoose.connect(DB_URL)
+            await mongoose.connect(mongoDB)
             console.log(`Server is running on: http://localhost:${PORT}`)
         })
     } catch (e) {
